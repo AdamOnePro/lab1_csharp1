b40ba1e1c9a46fb19531295579f1d8d42b6cbf1f
Slavick <adamone.ru@gmail.com>
2020-03-09 22:59:36 +0300

    hi

---
+++ lab1_csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int k = 9;
+            for(int n = 1; n<=5;n++)
+            {
+                number = (n * k) % 24 + 1;
+                Console.WriteLine (number);
+            }
+
+        }
+    }
+}
